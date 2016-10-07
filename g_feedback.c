61611e0cf6fb04258b99cd8ac564d5b06802ede7
Aleksandr Palamar <void995@gmail.com>
2016-10-07 19:52:39 +0300

    * More announcer message

--- g_feedback.c
+++ g_feedback.c
@@ -19,6 +19,38 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 #include "g_local.h"
 
+static const char *G_Feedback_PlaceString(int rank)
+{
+	static char	buffer[64];
+	char *place;
+
+	if (rank == 1) {
+		place = "1st";
+	} else if (rank == 2) {
+		place = "2nd";
+	} else if (rank == 3) {
+		place = "3rd";
+	} else if (rank == 11) {
+		place = "11th";
+	} else if (rank == 12) {
+		place = "12th";
+	} else if (rank == 13) {
+		place = "13th";
+	} else if (rank % 10 == 1) {
+		place = va("%ist", rank);
+	} else if (rank % 10 == 2) {
+		place = va("%ind", rank);
+	} else if (rank % 10 == 3) {
+		place = va("%ird", rank);
+	} else {
+		place = va("%ith", rank);
+	}
+
+	Q_snprintf(buffer, sizeof(buffer), "%s", place);
+
+	return buffer;
+}
+
 void G_Feedback_Init()
 {
 	gi.soundindex("misc/hit_3.wav");
@@ -52,10 +84,42 @@ void G_Feedback_ClientHit(edict_t *attacker, edict_t *victim, int damage_given)
 
 void G_Feedback_ClientKilled(edict_t *attacker, edict_t *victim)
 {
-	if (attacker->client && victim->client && attacker != victim) {
-		gi.sound(attacker, CHAN_FEEDBACK, gi.soundindex("misc/kill.wav"), 1.f, ATTN_STATIC, 0);
-		
-		gi.centerprintf(attacker, "You fragged %s", victim->client->pers.netname);
-		gi.centerprintf(victim, "Fragged by %s", attacker->client->pers.netname);
+	char attacker_message[MAX_STRING_CHARS];
+	char victim_message[MAX_STRING_CHARS];
+
+	int i, ranks_count, attacker_rank, victim_rank;
+	gclient_t *ranks[MAX_CLIENTS];
+
+	if (attacker->client && victim->client) {
+		memset(attacker_message, 0, MAX_STRING_CHARS);
+		memset(victim_message, 0, MAX_STRING_CHARS);
+
+		attacker_rank = -1;
+		victim_rank = -1;
+
+		ranks_count = G_CalcRanks(ranks);
+
+		for (i = 0; i < ranks_count; i++) {
+			if (ranks[i] == attacker->client) {
+				attacker_rank = i;
+			}
+
+			if (ranks[i] == victim->client) {
+				victim_rank = i;
+			}
+		}
+
+		if (attacker != victim) {
+			gi.sound(attacker, CHAN_FEEDBACK, gi.soundindex("misc/kill.wav"), 1.f, ATTN_STATIC, 0);
+
+			Q_snprintf(attacker_message, MAX_STRING_CHARS, "You fragged %s\n%s place with %d", victim->client->pers.netname, G_Feedback_PlaceString(attacker_rank + 1), attacker->client->resp.score);
+			Q_snprintf(victim_message, MAX_STRING_CHARS, "Fragged by %s\n", attacker->client->pers.netname);
+
+			gi.centerprintf(attacker, "%s", attacker_message);
+		}
+
+		Q_snprintf(victim_message, MAX_STRING_CHARS, "%s%s place with %d", victim_message, G_Feedback_PlaceString(victim_rank + 1), victim->client->resp.score);
+
+		gi.centerprintf(victim, "%s", victim_message);
 	}
 }
